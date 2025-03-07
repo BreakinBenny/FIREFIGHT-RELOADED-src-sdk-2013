e286aa88eb40a7e71c243a0a797d678a16f926ef
Bitl <bitlblox@gmail.com>
2025-03-07 10:27:24 -0700

    fake dual wield status in the hud if we previously dual wielded

--- sp/src/game/client/hl2/hud_weaponselection.cpp
+++ sp/src/game/client/hl2/hud_weaponselection.cpp
@@ -932,7 +932,7 @@ void CHudWeaponSelection::DrawLargeWeaponBox( C_BaseCombatWeapon *pWeapon, bool 
 			g_pVGuiLocalize->ConvertANSIToUnicode(weaponInfo.szPrintName, text, sizeof(text));
 		}
 
-		if (pWeapon->IsDualWielding())
+		if (pWeapon->DidPreviouslyDualWield() || pWeapon->IsDualWielding())
 		{
 			_snwprintf(text, sizeof(text) / sizeof(wchar_t) - 1, L"%s (x2)", text);
 		}
