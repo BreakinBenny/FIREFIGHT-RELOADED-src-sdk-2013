e17b3cba0a78b6653bfae47cc2d7fac3343c5cf8
Bitl <bitlblox@gmail.com>
2025-03-29 22:06:42 -0700

    Implement https://github.com/ValveSoftware/source-sdk-2013/pull/1033

--- sp/src/game/server/triggers.cpp
+++ sp/src/game/server/triggers.cpp
@@ -808,14 +808,17 @@ int CTriggerHurt::HurtAllTouchers( float dt )
 	touchlink_t *root = ( touchlink_t * )GetDataObject( TOUCHLINK );
 	if ( root )
 	{
-		for ( touchlink_t *link = root->nextLink; link != root; link = link->nextLink )
+		for (touchlink_t* link = root->nextLink; link != root && link != nullptr; link = link->nextLink)
 		{
-			CBaseEntity *pTouch = link->entityTouched;
-			if ( pTouch )
+			if (link)
 			{
-				if ( HurtEntity( pTouch, fldmg ) )
+				CBaseEntity* pTouch = link->entityTouched;
+				if (pTouch)
 				{
-					hurtCount++;
+					if (HurtEntity(pTouch, fldmg))
+					{
+						hurtCount++;
+					}
 				}
 			}
 		}
