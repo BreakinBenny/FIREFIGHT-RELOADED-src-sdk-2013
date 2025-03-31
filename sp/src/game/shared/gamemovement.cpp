bbe5ec3133f80e40827f3e69338e4144abd6d3af
Bitl <bitlblox@gmail.com>
2025-03-30 18:53:05 -0700

    Implement https://github.com/ValveSoftware/source-sdk-2013/pull/1166. update player movement code

--- sp/src/game/shared/gamemovement.cpp
+++ sp/src/game/shared/gamemovement.cpp
@@ -2812,7 +2812,7 @@ void CGameMovement::FullLadderMove()
 // Purpose: 
 // Output : int
 //-----------------------------------------------------------------------------
-int CGameMovement::TryPlayerMove( Vector *pFirstDest, trace_t *pFirstTrace )
+int CGameMovement::TryPlayerMove( Vector *pFirstDest, trace_t *pFirstTrace, float flSlideMultiplier /* = 0.f */)
 {
 	int			bumpcount, numbumps;
 	Vector		dir;
@@ -3002,12 +3002,12 @@ int CGameMovement::TryPlayerMove( Vector *pFirstDest, trace_t *pFirstTrace )
 				if ( planes[i][2] > 0.7  )
 				{
 					// floor or slope
-					ClipVelocity( original_velocity, planes[i], new_velocity, 1 );
+					ClipVelocity( original_velocity, planes[i], new_velocity, 1, flSlideMultiplier);
 					VectorCopy( new_velocity, original_velocity );
 				}
 				else
 				{
-					ClipVelocity( original_velocity, planes[i], new_velocity, 1.0 + sv_bounce.GetFloat() * (1 - player->m_surfaceFriction) );
+					ClipVelocity( original_velocity, planes[i], new_velocity, 1.0 + sv_bounce.GetFloat() * (1 - player->m_surfaceFriction), flSlideMultiplier);
 				}
 			}
 
@@ -3022,7 +3022,8 @@ int CGameMovement::TryPlayerMove( Vector *pFirstDest, trace_t *pFirstTrace )
 					original_velocity,
 					planes[i],
 					mv->m_vecVelocity,
-					1);
+					1, 
+					flSlideMultiplier);
 
 				for (j=0 ; j<numplanes ; j++)
 					if (j != i)
@@ -3507,7 +3508,7 @@ void CGameMovement::PushEntity( Vector& push, trace_t *pTrace )
 //			overbounce - 
 // Output : int
 //-----------------------------------------------------------------------------
-int CGameMovement::ClipVelocity( Vector& in, Vector& normal, Vector& out, float overbounce )
+int CGameMovement::ClipVelocity( Vector& in, Vector& normal, Vector& out, float overbounce, float flRedirectCoeff /* = 0.f */)
 {
 	float	backoff;
 	float	change;
@@ -3524,7 +3525,8 @@ int CGameMovement::ClipVelocity( Vector& in, Vector& normal, Vector& out, float 
 	
 
 	// Determine how far along plane to slide based on incoming direction.
-	backoff = DotProduct (in, normal) * overbounce;
+	float flBlocked = DotProduct(in, normal);
+	backoff = flBlocked * overbounce;
 
 	for (i=0 ; i<3 ; i++)
 	{
@@ -3538,6 +3540,16 @@ int CGameMovement::ClipVelocity( Vector& in, Vector& normal, Vector& out, float 
 	{
 		out -= ( normal * adjust );
 //		Msg( "Adjustment = %lf\n", adjust );
+	}
+
+	if (flRedirectCoeff > 0.f)
+	{
+		// Redirect clipped velocity along angle of movement
+		float flLen = out.Length();
+		if (flLen != 0.0f)
+		{
+			out *= (-1.f * flBlocked * flRedirectCoeff + flLen) / flLen;
+		}
 	}
 
 	// Return blocking flags.
