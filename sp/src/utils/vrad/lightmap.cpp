a8bc28ba867a159531019dfd6c15a54f097983a5
Bitl <bitlblox@gmail.com>
2025-03-30 18:58:16 -0700

    Implement https://github.com/ValveSoftware/source-sdk-2013/pull/1116

--- sp/src/utils/vrad/lightmap.cpp
+++ sp/src/utils/vrad/lightmap.cpp
@@ -1666,9 +1666,6 @@ void ExportDirectLightsToWorldLights()
 
 #define CONSTANT_DOT (.7/2)
 
-#define NSAMPLES_SUN_AREA_LIGHT 30							// number of samples to take for an
-                                                            // non-point sun light
-
 // Helper function - gathers light from sun (emit_skylight)
 void GatherSampleSkyLightSSE( SSE_sampleLightOutput_t &out, directlight_t *dl, int facenum, 
 							 FourVectors const& pos, FourVectors *pNormals, int normalCount, int iThread,
@@ -1693,7 +1690,7 @@ void GatherSampleSkyLightSSE( SSE_sampleLightOutput_t &out, directlight_t *dl, i
 	int nsamples = 1;
 	if ( g_SunAngularExtent > 0.0f )
 	{
-		nsamples = NSAMPLES_SUN_AREA_LIGHT;
+		nsamples = g_SunSamplesAreaLight;	// number of samples to take for an non-point sun light
 		if ( do_fast || force_fast )
 			nsamples /= 4;
 	}
