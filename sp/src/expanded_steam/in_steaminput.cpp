568bc87703d30d545ee48bb323b826a01eec78af
Bitl <bitlblox@gmail.com>
2025-12-10 09:52:31 -0700

    Revert "Update in_steaminput.cpp"
    This reverts commit e63c6b58f8993b62859f2e8a8904ffcf0bec9243.

--- sp/src/expanded_steam/in_steaminput.cpp
+++ sp/src/expanded_steam/in_steaminput.cpp
@@ -429,7 +429,11 @@ void CSource2013SteamInput::InitSteamInput()
 #define ACTION_MANIFEST_MOD					"steam_input/action_manifest_mod.vdf"
 #define ACTION_MANIFEST_RELATIVE_HL2		"%s/../Half-Life 2/steam_input/action_manifest_hl2.vdf"
 #define ACTION_MANIFEST_RELATIVE_PORTAL		"%s/../Portal/steam_input/action_manifest_hl2.vdf"
+#if defined( LINUX )
+#define ACTION_MANIFEST_RELATIVE_FR			"%s/../FIREFIGHT RELOADED/controller_config/action_manifest_firefightreloaded.vdf"
+#else
 #define ACTION_MANIFEST_RELATIVE_FR			"%s/controller_config/action_manifest_firefightreloaded.vdf"
+#endif
 
 void CSource2013SteamInput::InitActionManifest()
 {
